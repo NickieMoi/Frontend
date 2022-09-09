7732d3ce89af1c17d713016d0a34a0b5cff212e7
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-09-09 23:36:45 +0300

    adds new files and images

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -1,5 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Route, Switch } from "react-router-dom"
+import NavBar from './Components/NavBar';
+import Home from './Components/Home';
+import OurRooms from './Components/OurRooms';
+import About from './Components/About';
+import Restaurant from './Components/Hotel';
+import Contact from './Components/Contact';
 
 function App() {
   return (
@@ -18,8 +26,25 @@ function App() {
           Learn React
         </a>
       </header>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/aboutUs">
+          <About />
+        </Route>
+        <Route exact path="/ourRooms">
+          <OurRooms />
+        </Route>
+        <Route exact path="/restaurant">
+          <Restaurant />
+        </Route>
+        <Route exact path="/contactUs">
+          <Contact />
+        </Route>
+      </Switch>
     </div>
   );
 }
-
 export default App;
