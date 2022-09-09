0f48e6b78bec807e35f478924108e242f7304665
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-09-10 00:25:23 +0300

    add a file to handle reviews

---
+++ my-app-frontend/src/components/NewReview.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+function NewReview({onAddReview}) {
+    const [formData, setFormData] = useState({
+        body: ""
+    })
+    function handleChange(e){
+        setFormData({...formData, [e.target.name]: e.target.value})
+    }
+    function handleSubmit(e){
+        e.preventDefault();
+        fetch("http://localhost:9292/reviews", {
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json",
+            },
+            body: JSON.stringify({body:formData.body})
+    })
+           .then(response => response.json())
+           .then((data) => {
+           onAddReview(data)
+           setFormData({...formData,body:""})
+    })
+}  
+  return (
+    <div>
+        <form className='review-form' onSubmit={handleSubmit}></form>
+        <input className="input" type="text" name="body" placeholder='Add Your Comment' value={formData.body} onChange={handleChange} />
+            <button type="submit" className='submit'>Submit</button>
+    </div>
+  )
+}
+export default NewReview;
