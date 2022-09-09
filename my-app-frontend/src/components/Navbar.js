d375b2d1fc1faae7d7af9132b8a79e891661d7b4
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-09-10 00:00:12 +0300

    adds the navbar styling

---
+++ my-app-frontend/src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { NavLink } from "react-router-dom"
+const navLinks = {
+    display: "inline",
+    textDecoration: "none",
+    fontSize: "18px",
+    marginLeft: "15rem"
+}
+function NavBar(){
+    return (
+        <div className="nav">
+            <div>
+                <h3 style={{fontSize: "23px", marginLeft: "2rem"}}>ReelWorld</h3>
+            </div>
+            <div>
+                <NavLink to="/" style={navLinks}>
+                    Home
+                </NavLink>
+                <NavLink to="customerReviews" style={navLinks}>
+                    Reviews
+                </NavLink>
+
+                <NavLink to="contactUs" style={navLinks}>
+                    Contact Us
+                    Contact us
+                </NavLink>
+            </div>
+        </div>
+    )
+}
+export default NavBar;
