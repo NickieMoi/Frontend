e758c3b4bce0f10d9e4a56a7bce281865f2f0f90
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-09-09 23:56:47 +0300

    adds the homepage

---
+++ my-app-frontend/src/components/Home.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react'
+import hotel from "../images/hotel1.jpg"
+import hotel2 from "../images/hotel2.jpg"
+function Home() {
+    const [hotels, setHotels] = useState([]);
+    return (
+        <div className='bg-page'>
+          <div className='intro'>
+          <div className='p-home'>
+            <img src={hotel2} alt="hotel2" className='hotel2' style={{width: "400px", height: "350px"}}/>
+            <p>Welcome to ReelWorld. <br></br>ReelWorld hotel 5 minutes by car from Nairobi CBD. <br></br>it offers free WIFI and parking.</p>
+          </div>
+          <div>
+          <img src={hotel} alt="hotel1" className="hotel1" style={{width: "850px", height: "600px", marginLeft: "3rem"}}/>
+            <img src={hotel} alt="hotel1" className="hotel1" style={{width: "850px", height: "600px", marginLeft: "3rem"}}/>
+          </div>
+          </div>
+            <header>
+              { hotels.map((hotel) => (
+                  <ol id={hotel.id} key={hotel.id} >
+                  <p>Name: {hotel.name}</p>
+                  <p>Location:{hotel.location}</p>
+                  <p>Price: {hotel.price}</p>
+                  </ol>
+              ))}
+            </header>
+        </div>
+      )
+    }
+  export default Home;
