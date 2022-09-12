125308cb1e1cdcb4ead816bb8dbc332d71d1ce08
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-09-12 22:57:07 +0300

    imports the react

--- my-app-frontend/src/components/Home.js
+++ my-app-frontend/src/components/Home.js
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import hotel from "./hotel1.jpeg"
 import hotel2 from "./hotel2.jpeg"
 function Home() {
-    const [hotels, setHotels] = useState([]);
+    // const [hotels, setHotels] = useState([]);
     return (
         <div className='bg-page'>
           <div className='intro'>
           <div className='p-home'>
             <img src={hotel2} alt="hotel2" className='hotel2' style={{width: "400px", height: "350px"}}/>
-            <p>Welcome to ReelWorld. <br></br>ReelWorld hotel 5 minutes by car from Nairobi CBD. <br></br>it offers free WIFI and parking.</p>
+            <p>Welcome to . <br></br>Miami hotel 5 minutes by car from Kericho CBD. <br></br>it offers free WIFI and parking.</p>
           </div>
           <div>
           <img src={hotel} alt="hotel1" className="hotel1" style={{width: "850px", height: "600px", marginLeft: "3rem"}}/>
             <img src={hotel} alt="hotel1" className="hotel1" style={{width: "850px", height: "600px", marginLeft: "3rem"}}/>
           </div>
           </div>
-            <header>
+            {/* <header>
               { hotels.map((hotel) => (
                   <ol id={hotel.id} key={hotel.id} >
                   <p>Name: {hotel.name}</p>
@@ -23,7 +23,7 @@ function Home() {
                   <p>Price: {hotel.price}</p>
                   </ol>
               ))}
-            </header>
+            </header> */}
         </div>
       )
     }
