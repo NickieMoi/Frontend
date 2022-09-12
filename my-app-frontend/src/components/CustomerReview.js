d913f703a243145ec1c29636c6c75f4a3d35bc24
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-09-12 22:53:45 +0300

    fetchs reviw data

--- my-app-frontend/src/components/CustomerReview.js
+++ my-app-frontend/src/components/CustomerReview.js
@@ -1,24 +1,38 @@
 import React, { useState, useEffect } from 'react'
 import NewReview from './NewReview';
 import Review from './ReviewDelete';
+
 function CustomerReview() {
   const [reviews, setReviews] = useState([]);
+
   useEffect(() => {
-    fetch("http://localhost:9292/reviews")
+    fetch("https://hotel-backend-app.herokuapp.com/reviews")
     .then((response) => response.json())
     .then((data) =>{
       setReviews(data)
     })
   },[]);
+
+
   function handleAddReview(newReview) {
     setReviews([...reviews, newReview]);
   }
+
   function handleDeleteReview(deletedReview){
     const updatedReviews = reviews.filter((review) => review.id !== deletedReview.id)
     setReviews(updatedReviews)
   }
+
+  // const showReview = reviews.map((review) =>(
+  //   <ol key={review.id}>
+  //    Comment:{review.body}
+  //    <button onClick={() => handleDeleteReview(review.id)}>Del Rev</button>
+  //   </ol>
+  // ))
+  
   return (
     <div className='cust-rev'>
+      <h3>Our Customer Reviews</h3>
       <ul className="Reviews">
         {reviews.map((review) => (
           <Review id={review.id} key={review.id} review={review}  onDelete={handleDeleteReview} />
@@ -28,4 +42,5 @@ function CustomerReview() {
     </div>
   )
 }
+
 export default CustomerReview;
