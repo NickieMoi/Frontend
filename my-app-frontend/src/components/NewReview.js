acc33cb3ab5b70aab3d1ba3c8a1d933bdb990b35
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-09-12 23:04:25 +0300

    adds the new reviw routes

--- my-app-frontend/src/components/NewReview.js
+++ my-app-frontend/src/components/NewReview.js
@@ -1,17 +1,22 @@
 import React, { useState } from 'react'
+
 function NewReview({onAddReview}) {
     const [formData, setFormData] = useState({
         body: ""
     })
+
     function handleChange(e){
         setFormData({...formData, [e.target.name]: e.target.value})
     }
+
     function handleSubmit(e){
         e.preventDefault();
+
         fetch("http://localhost:9292/reviews", {
             method: "POST",
             headers: {
                 "Content-Type":"application/json",
+                'Accept': 'application/json'
             },
             body: JSON.stringify({body:formData.body})
     })
@@ -23,10 +28,13 @@ function NewReview({onAddReview}) {
 }  
   return (
     <div>
-        <form className='review-form' onSubmit={handleSubmit}></form>
-        <input className="input" type="text" name="body" placeholder='Add Your Comment' value={formData.body} onChange={handleChange} />
+        <p style={{marginLeft:"50px"}}>Add Your Review</p>
+        <form className='review-form' onSubmit={handleSubmit}>
+            <input className="input" type="text" name="body" placeholder='Add Your Comment...' value={formData.body} onChange={handleChange} />
             <button type="submit" className='submit'>Submit</button>
+        </form>
     </div>
   )
 }
+
 export default NewReview;
