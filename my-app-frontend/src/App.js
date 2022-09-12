930131a51e5f4cfb0b969a7aeb7aaee389bb663d
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-09-12 22:48:28 +0300

    updates the handles of app js

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -1,18 +1,20 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavBar from "./Components/NavBar";
-import Home from "./Components/Home";
-import Contact from "./Components/Contact";
-import CustomerReview from "./Components/CustomerReview";
-import Review from "./Components/ReviewDelete";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import CustomerReview from "./components/CustomerReview"; 
+// import Review from "./components/ReviewDelete";
+// import NewReview from "./components/NewReview";
 
 function App() {
   return (
     <div className="App">
+      {/* <Router />
       <NavBar />
-      <Router>
-        <Route exact path="./" element={<NavBar/>}></Route>
+      <Routes>
+        <Route exact path="./" element={<NavBar />}></Route>
 
         <Route exact path="contactUs" element={<Contact />}></Route>
         <Route
@@ -20,9 +22,22 @@ function App() {
           path="customerReviews"
           element={<CustomerReview />}
         ></Route>
-        <Review />
+
+      </Routes>
+    </Router> */}
+    <Router>
+      <NavBar />
+      <Routes>
+      <Route exact path='/contactUs' element={<Contact/>} />
+        <Route path='/' element={<Home/>} />
+        <Route path='/customerReviews' element={<CustomerReview />} />
+        {/* <Route path='/newreview' element={<NewReview/>}/> */}
+        <Route path='/review' element={<CustomerReview/>}/>
+        {/* <Route path='/cars' element={<Cars />} /> */}
+        </Routes>
       </Router>
-    </div>
+
+  </div>
   );
 }
 
